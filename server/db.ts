ddb2e0b91dc62744bdefc9c5f47ab30d87c38e2c
ByteBlink <jaakupienes@gmail.com>
2024-03-07 17:59:28 +0100

    fix: db

--- server/db.ts
+++ server/db.ts
@@ -1,11 +1,10 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const DBConnectionString: string =
-  process.env.DBConnectionString || "mongodb://localhost:27017/reciplease";
+const DBConnectionString: string = 'mongodb://localhost:27017/reciplease';
 
 mongoose
   .connect(DBConnectionString)
-  .then(() => console.log("Reciplease database connected"))
+  .then(() => console.log('Reciplease database connected'))
   .catch((err: Error) => console.error(`Database failed to connect: ${err}`));
 
-  export default mongoose;
+export default mongoose;
