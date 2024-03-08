a94cd4e60c2247149846fde411dbbd17ea361e78
Gabe Mata <gabemata@gmail.com>
2024-03-08 12:24:03 +0100

    feat: main and app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,8 @@ import App from "./App.js";
 import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+//Other people have answered that you should add a null-check, but Typescript also has a non-null assertion that you can use when you are sure that the value is never null by adding the ! operator to the end of your statement:
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
