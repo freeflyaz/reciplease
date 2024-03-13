6f170b3edbe53aeb081e8e1629819033db9e49f1
Sebastian Arteaga Ronga <sarteaga0415@gmail.com>
2024-03-13 09:07:40 -0500

    feat: test working, cleaning

---
+++ client/test/pages/dashboard/add-and-search-bar.test.tsx
@@ -0,0 +1,25 @@
+import { describe, it, expect, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import React from 'react';
+import AddAndSearchBar from '../../../src/pages/dashboard/add-and-search-bar';
+import { BrowserRouter as Router } from "react-router-dom";
+
+describe('AddAndSearchBar component', () => {
+  it('renders the add recipe button and the search input', () => {
+    const mockSetQuery = vi.fn(); // Mock the setQuery function
+
+    render(
+      <Router>
+        <AddAndSearchBar query="" setQuery={mockSetQuery} />
+      </Router>
+    );
+
+    const addButton = screen.getByRole('button', { name: /add recipe/i });
+    const searchInput = screen.getByPlaceholderText('Search here...');
+
+    expect(addButton).toBeInTheDocument();
+    expect(searchInput).toBeInTheDocument();
+  });
+
+});
